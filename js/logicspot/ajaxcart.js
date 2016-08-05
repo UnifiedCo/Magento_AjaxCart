c802f3da196944fd12efb52e5e10b22074a0c5ca
Kamil Szewczyk <kamil@logicspot.com>
2016-08-05 17:44:45 +0100

    moving files to correct places, cleaning css, working on objectg instead of simple jquery to allow configurable callbacks

--- js/logicspot/ajaxcart.js
+++ js/logicspot/ajaxcart.js
@@ -1,27 +1,38 @@
-jQuery.fn.center = function (absolute) {
-    return this.each(function () {
-        var t = jQuery(this);
-        t.css({
-            position: absolute ? 'absolute' : 'fixed',
-            left: '50%',
-            top: '50%',
-            zIndex: '1000'
-        }).css({
-            marginLeft: '-' + (t.outerWidth() / 2) + 'px',
-            marginTop: '-' + (t.outerHeight() / 2) + 'px'
+;(function($){
+    $.fn.ajaxCart = function(options) {
+
+        /**
+         * Defaults
+         */
+        var defaults = {
+            selectors: {
+                addButton: '.btn-cart'
+            },
+            complete: function () {}
+
+        };
+
+        /**
+         * Options
+         */
+        var options = $.extend(defaults, options);
+
+
+        /**
+         * Return each object
+         */
+        return this.each(function() {
+
+            options.init($(this).attr('id'));
+
         });
 
-        if (absolute) {
-            t.css({
-                marginTop: parseInt(t.css('marginTop'), 10) + jQuery(window).scrollTop(),
-                marginLeft: parseInt(t.css('marginLeft'), 10) + jQuery(window).scrollLeft()
-            });
-        }
-    });
-};
+    };
+
+})(jQuery);
+
 
 jQuery(document).ready(function () {
-    jQuery('.ajaxcart-element').center();
     if (jQuery('.btn-cart').size() > 0) {
         var onclickHolder = {}; //IE8 fix
 
